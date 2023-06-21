cf3f9d48b93110e562bb450989e88bffcba4d830
Rane-e <135345418+Rane-e@users.noreply.github.com>
2023-06-21 16:47:13 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RKIS_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("RKIS_8");
+        }
+    }
+}
